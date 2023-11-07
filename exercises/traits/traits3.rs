e2bb72d1f3a210105597ce7cb51d7d0812a85635
xueduanwei <xueduanwei@bytedance.com>
2023-11-07 17:54:00 +0800

    done test

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -8,10 +8,12 @@
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        return "Some information".to_string()
+    }
+    
 }
 
 struct SomeSoftware {
@@ -24,6 +26,7 @@ struct OtherSoftware {
 
 impl Licensed for SomeSoftware {} // Don't edit this line
 impl Licensed for OtherSoftware {} // Don't edit this line
+
 
 #[cfg(test)]
 mod tests {
